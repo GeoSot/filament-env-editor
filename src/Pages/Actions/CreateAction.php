77c87f94b84399cc7dae5a018d0538f039006497
GeoSot <geo.sotis@gmail.com>
2026-02-14 01:14:17 +0200

    feat: upgrade filament/filament to v5.0 and enhance action handling

--- src/Pages/Actions/CreateAction.php
+++ src/Pages/Actions/CreateAction.php
@@ -3,6 +3,8 @@
 namespace GeoSot\FilamentEnvEditor\Pages\Actions;
 
 use Filament\Actions\Action;
+use Filament\Actions\Concerns\CanCustomizeProcess;
+use Filament\Forms\Components\Field;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Support\Colors\Color;
@@ -17,15 +19,56 @@ use Illuminate\Support\Str;
 
 class CreateAction extends Action
 {
+    use CanCustomizeProcess;
+
     protected function setUp(): void
     {
         parent::setUp();
 
         $this->label(fn (): string => __('filament-env-editor::filament-env-editor.actions.add.title'));
-        $this->modalHeading(fn (
-        ): string => __('filament-env-editor::filament-env-editor.actions.add.modalHeading'));
+        $this->modalHeading(fn (): string => __('filament-env-editor::filament-env-editor.actions.add.modalHeading'));
 
-        $this->schema([
+        $this->color(Color::Teal);
+        $this->modalWidth(Width::Large);
+
+        $this->schema($this->getFormFields());
+
+        $this->action(function (array $data, ViewEnv $page) {
+            try {
+                $options = Arr::get($data, 'index')
+                    ? ['index' => Arr::get($data, 'index')]
+                    : [];
+
+                $result = EnvEditor::addKey(
+                    $data['key'],
+                    $data['value'],
+                    $options
+                );
+
+                $this->successNotificationTitle(
+                    __(
+                        'filament-env-editor::filament-env-editor.actions.add.success.title',
+                        ['name' => $data['key']]
+                    )
+                );
+
+                $result ? $this->success() : $this->failure();
+
+                $page->triggerRefresh();
+            } catch (EnvException $exception) {
+                $this->failureNotificationTitle($exception->getMessage());
+                $this->failure();
+                $this->halt();
+            }
+        });
+    }
+
+    /**
+     * @return list<Field>
+     */
+    private function getFormFields(): array
+    {
+        return [
             TextInput::make('key')
                 ->label(__('filament-env-editor::filament-env-editor.actions.add.form.fields.key'))
                 ->required(),
@@ -36,36 +79,7 @@ class CreateAction extends Action
                 ->helperText(__('filament-env-editor::filament-env-editor.actions.add.form.helpText.index'))
                 ->options(fn () => $this->getExistingKeys())
                 ->searchable(),
-        ]);
-
-        $this->action(function (array $data, ViewEnv $page) {
-            $result = false;
-            try {
-                $options = Arr::get($data, 'index')
-                    ? ['index' => Arr::get($data, 'index')]
-                    : [];
-                $result = EnvEditor::addKey(
-                    $data['key'],
-                    $data['value'],
-                    $options
-                );
-                $page->refresh();
-                $this->successNotificationTitle(fn (
-                ): string => __(
-                    'filament-env-editor::filament-env-editor.actions.add.success.title',
-                    ['name' => $data['key']]
-                ));
-            } catch (EnvException $exception) {
-                $this->failureNotificationTitle($exception->getMessage());
-                $this->failure();
-                $this->halt();
-            }
-
-            $result ? $this->success() : $this->failure();
-        });
-
-        $this->color(Color::Teal);
-        $this->modalWidth(Width::Large);
+        ];
     }
 
     /**
@@ -76,7 +90,11 @@ class CreateAction extends Action
         return EnvEditor::getEnvFileContent()
             ->filter(fn (EntryObj $obj) => !$obj->isSeparator())
             ->groupBy('group')
-            ->keyBy(fn (Collection $c): string => Str::of($c->first()->key)->before('_')->remove('#')->lower()->prepend('--- '))
+            ->keyBy(
+                fn (Collection $c): string => Str::of($c->first()->key)->before('_')->remove('#')->lower()->prepend(
+                    '--- '
+                )
+            )
             ->map(fn (Collection $c): Collection => $c->mapWithKeys(fn (EntryObj $obj) => [$obj->index => $obj->key]))
             ->toArray();
     }
